6c10cad893edacd312e72c73acb86a57e5f82d34
Nico Bailon <nico.bailon@gmail.com>
2014-10-09 21:38:12 -0700

    Tweaked build process (Gruntfile.js) Added proper page class to model detail template Page banner titles: changed max-width so the title text looks the same as the mock

--- Gruntfile.js
+++ Gruntfile.js
@@ -147,5 +147,5 @@ module.exports = function(grunt) {
   // Default tasks
   grunt.registerTask('default', ['includes:default','compass']);
   // Build tasks
-  grunt.registerTask('build', ['compass','uncss','cssmin','includes:build','processhtml','imagemin','uglify']);
+  grunt.registerTask('build', ['includes:default','compass','uncss','cssmin','includes:build','processhtml','imagemin','uglify']);
 };
